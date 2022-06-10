5dc4b565d18ecd32ef17e8a1c0ec2f5fc7f09cc0
Aayush S C <78192713+animemandir@users.noreply.github.com>
2022-06-10 18:17:26 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '430e6b3a04ba45e8ae558a386551481b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
+  rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'Spookis Entertainment',
-  domain: 'transitivebullsh.it',
+  domain: 'spookis.netlify.app',
   author: 'spookis',
 
   // open graph metadata (optional)
